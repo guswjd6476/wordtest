538d4235eaa5898d737d100d94f72bfdd837ec29
hyunjung <sspp4555@naver.com>
2024-08-20 02:30:49 +0900

    1 commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { AuthProvider } from './AuthProvider';
+import { AuthProvider } from '../AuthProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { useAuth } from './AuthProvider';
+import { useAuth } from '../AuthProvider';
 
 interface ItemWithExpireTime {
     value: string;
